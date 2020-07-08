f251f906d522d44abf877d74a594ca5a0672bffc
ameypatwardhan <ameyap82@gmail.com>
2020-07-08 09:30:13 +0530

    first commit

---
+++ src/test/java/com/example/abctest/AbctestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbctestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
